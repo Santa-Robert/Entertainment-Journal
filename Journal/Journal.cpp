876eeae6aae9232a62f9d4c8c12e6311c5a757a3
Robi <santa.robert.2011@gmail.com>
2025-11-05 20:29:54 +0200

    Initial commit, also added a basic list of commands

---
+++ Journal/Journal.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+void help_list()
+{
+    cout<<"-add *category*  - adds content to desired category\n"
+          "-remove *category* - removes content from desired category\n"
+          "-status *category* - lists the content of the desired category\n"
+          "*command* +to_watch - uses category of wishlisted media\n"
+          "*command* +watched - uses category of previously seen media\n"
+          "*command* +time_stamp - uses category of currently seen Season, Episode, and time stamp (bookmark)\n"<<endl;
+    
+}
+
+int main()
+{
+    string input;
+
+    cout<<"enter '?' for command list"<<endl;
+    
+    cin >> input;
+
+    if (input == "?" )
+    {
+        help_list();
+    }
+
+    
+}
