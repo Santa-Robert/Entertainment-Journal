64289f587d043801580bc7e157707d77881181cc
Robi <santa.robert.2011@gmail.com>
2025-11-23 22:08:32 +0200

    made the program react to most commands, still doesn't save.

--- Journal/Journal.cpp
+++ Journal/Journal.cpp
@@ -1,30 +1,78 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
 
 using namespace std;
 
 void help_list()
 {
-    cout<<"-add *category*  - adds content to desired category\n"
-          "-remove *category* - removes content from desired category\n"
-          "-status *category* - lists the content of the desired category\n"
-          "*command* +to_watch - uses category of wishlisted media\n"
-          "*command* +watched - uses category of previously seen media\n"
-          "*command* +time_stamp - uses category of currently seen Season, Episode, and time stamp (bookmark)\n"<<endl;
+    cout<<"add *category*  - adds content to desired category\n"
+          "rm *category* - removes content from desired category\n"
+          "status *category* - lists the content of the desired category, use '.' to list all\n"
+          "*command* -to_watch - uses category of wishlisted media\n"
+          "*command* -watched - uses category of previously seen media\n"
+          "*command* -time_stamp - uses category of currently seen Season, Episode, and time stamp (bookmark)\n"<<endl;
     
 }
-//test
+
 int main()
 {
-    string input;
+    
+    string save[3];
 
     cout<<"enter '?' for command list"<<endl;
     
-    cin >> input;
+    cin>>save[0];
 
-    if (input == "?" )
+    if (save[0] == "?")
     {
         help_list();
+        cin>>save[0];
+    }
+    
+    cin>>save[1];
+    cin>>save[2];
+    
+    if (save[0]=="add")
+    {
+        if (save[1]=="-to_watch")
+        {
+            cout<<endl;
+            cout<<"saved\"" << save[2]<<"\" to wishlist!" <<endl; 
+        }
+        else if (save[1]=="-watched")
+        {
+            cout<<endl;
+            cout<<"saved\"" << save[2]<<"\" to trophy list!" <<endl;
+        }
+        else if (save[1]=="-time_stamp")
+        {
+            cout<<endl;
+            cout<<"saved \"" << save[2]<<"\" to bookmark!" <<endl;
+        }
+    }
+    if (save[0]=="rm")
+    {
+        if (save[1]=="-to_watch")
+        {
+            cout<<endl;
+            cout<<"removed\"" << save[2]<<"\" from wishlist!" <<endl;
+        }
+        else if (save[1]=="-watched")
+        {
+            cout<<endl;
+            cout<<"removed\"" << save[2]<<"\" from trophy list!" <<endl;
+        }
+        else if (save[1]=="-time_stamp")
+        {
+            cout<<endl;
+            cout<<"removed \"" << save[2]<<"\" from bookmark!" <<endl;
+        }
     }
 
+    
+    
     
 }
